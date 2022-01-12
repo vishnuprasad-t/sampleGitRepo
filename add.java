e78dc87878290f276f2cbcc5387a8bf3c080abcc
= <=>
2022-01-12 19:54:35 +0530

    Initial commit

---
+++ add.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class add {
+	int x=0, y=0, sum;
+
+	public void adding (int a, int b) {
+		sum = a+b;
+		System.out.println("Sum of "+a+" and "+b+ " is "+sum);
+	}
+
+
+}
---
+++ mainAdd.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class mainAdd {
+
+	public static void main(String[] args) {
+		System.out.println("Program to add 2 numbers");
+		int a=10, b=15;
+		
+		add obj = new add();
+		obj.adding(a, b);
+
+	}
+
+}
